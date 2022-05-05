d162c6c7ccab65012331932ac8d809b94c8c04f8
thamara31 <tmagliano27@gmail.com>
2022-05-05 17:27:46 -0300

    first commit

---
+++ main.java
@@ -0,0 +1,28 @@
+package dioexercicio;
+
+/** classe principal dos exercícios de aula 1 de métodos.
+ */
+
+
+public class main {
+
+
+    public static void main(String[] args){
+
+        //Calculadora
+        System.out.println("exercicio calculadora");
+        Calculadora.soma(3,1);
+        Calculadora.subtracao(10,3);
+        Calculadora.multiplicacao(7,3);
+        Calculadora.divisao(10,2);
+
+
+
+
+
+
+
+
+
+    }
+}
